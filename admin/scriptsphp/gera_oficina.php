9cd3d2c9a0ed049cf631fd71d3042623404f8b97
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2020-10-11 15:58:40 -0300

    Bug Corrigido. Qrcode redirecionando para a url certa!

--- admin/scriptsphp/gera_oficina.php
+++ admin/scriptsphp/gera_oficina.php
@@ -13,20 +13,12 @@
     $data = date('Y-m-d H:i:s');
     $dados = $_SESSION['dados'];
 
-    // QR CODE
-
-    $url_atual = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-
-        $api_qrcode = 'https://chart.googleapis.com/chart?';
-        $api_qrcode .= '&cht=qr';
-        $api_qrcode .= '&chl=' . $url_atual;
-        $api_qrcode .= '&chs=120x120';
 
     $sql_insere_oficina = "INSERT INTO oficinas(nome_oficina, descricao_oficina, professor_oficina, 
     disciplina_oficina, sala_oficina, limite_vagas_oficina, requisitos_oficina, dia_oficina, hora_oficina,
-    hora_criacao_oficina, user_criador_oficina, status_oficina, vagas_restantes_oficina, qr_code) VALUES
+    hora_criacao_oficina, user_criador_oficina, status_oficina, vagas_restantes_oficina) VALUES
     ('$informacoes_oficina[0]', '$informacoes_oficina[1]','$informacoes_oficina[2]','$informacoes_oficina[3]','$informacoes_oficina[4]',
-    '$informacoes_oficina[5]','$informacoes_oficina[6]','$informacoes_oficina[7]','$informacoes_oficina[8]', '$data', '$dados[1]', true, 0, '$api_qrcode')";
+    '$informacoes_oficina[5]','$informacoes_oficina[6]','$informacoes_oficina[7]','$informacoes_oficina[8]', '$data', '$dados[1]', true, 0)";
     $query_sql_insere_oficina = mysqli_query($conn, $sql_insere_oficina);
 
         if($query_sql_insere_oficina){
@@ -36,6 +28,16 @@
             $query_dados_oficina = mysqli_query($conn, $sql_dados_oficina);
             $dados_oficina = mysqli_fetch_array($query_dados_oficina);
 
+            // API QRCODE
+            $url_oficina = "http://localhost/SIME/oficinas/oficinas.php?id_oficina=$dados_oficina[0]";
+
+            $api_qrcode = 'https://chart.googleapis.com/chart?';
+            $api_qrcode .= '&cht=qr';
+            $api_qrcode .= '&chl=' . $url_oficina;
+            $api_qrcode .= '&chs=120x120';
+
+            $sql_insercao_qr_code = "UPDATE oficinas SET qr_code = '$api_qrcode' WHERE id_oficina = $dados_oficina[0]";
+            $insercao_qr_code = mysqli_query($conn, $sql_insercao_qr_code);
             $_SESSION['id_da_oficina'] = $dados_oficina[0];
             header('location: ../criaroficina.php');
         } else{
